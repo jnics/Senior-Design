6bbc51525ff25e34c47b7ba9fd59ccdd2ef6d1bb
jnics <jnics23@gmail.com>
2015-04-23 15:15:42 -0400

    Updated Breakout Class

--- Breakout.java
+++ Breakout.java
@@ -1,15 +1,7 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.applet.*;
-import java.lang.Math;
 import java.net.InetAddress;
-import java.net.URL;
 import java.util.Random;
-import java.io.*;
-
-import sun.audio.*;
-
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -50,8 +42,8 @@ public class Breakout extends Applet implements Runnable {
 	private Ball ball;
 	private BlockHolder blocks;
 	private Paddle paddle;
-	private int games = 0;
-	
+
+	// Initial display of the Breakout game, which is only called when the window is first opened
 	public void init() {
 		buffer = createImage(MAXX, MAXY);
 		gContext = buffer.getGraphics();
@@ -65,10 +57,9 @@ public class Breakout extends Applet implements Runnable {
 		blocks.draw(gContext);
 		new Sound(this);
 		Sound.backGround.loop();
-
 	}
 
-	
+	// Begins the game
 	public void start() {
 		if(animate == null) {
 			animate = new Thread(this);
@@ -76,8 +67,10 @@ public class Breakout extends Applet implements Runnable {
 		}
 	}
 	
+	// Empty is used in other classes
 	public void restart() {	}
 		
+	// Stops the game from continuing
 	public void stop() {
 		if(animate != null) {
 			animate.stop();
@@ -85,22 +78,17 @@ public class Breakout extends Applet implements Runnable {
 		}
 	}
 
-	
+	// Called to create the graphical representation of the game
 	public void paint(Graphics g) {
 		super.paint(g);
 		try {
 			colorCheck();
-			g.drawImage(buffer, 0, 0, MAXX, MAXY, this);
-			if(games < 0){
-				Sound.endGame.stop();
-				Sound.backGround.loop();
-			}
-			games++;
+			g.drawImage(buffer, 0, 0, MAXX, MAXY, this);			
 		}
 		catch(Exception e) {}
 	}
 
-	
+	// Starts and runs the thread that is the game 
 	public void run() {
 		try {
 			showStatus(InetAddress.getLocalHost().toString());
@@ -141,11 +129,12 @@ public class Breakout extends Applet implements Runnable {
 		}
 	}
 
+	//method that is called when we see a repaint(); call
 	public void update(Graphics g) {
 		paint(g);
 	}
 
-
+	// Sets the jobs for keys
 	public boolean keyDown(Event e, int key) {
 		if(key == 1004 && ballready) {
 			showStatus("Press P to pause. Score: " + Breakout.score);
@@ -156,13 +145,13 @@ public class Breakout extends Applet implements Runnable {
 		if(key == 112) {
 			if(!paused) {
 				paused = true;
-				//animate.suspend();
+				animate.suspend();
 				showStatus("Press P to unpause Score: " + Breakout.score);
 			}
 			else {
 				showStatus("Press P to pause Score: " + Breakout.score);
 				paused = false;
-				//animate.resume();
+				animate.resume();
 			}	
 		}
 		if(key == 1006)
@@ -172,7 +161,7 @@ public class Breakout extends Applet implements Runnable {
 		return true;
 	}
 
-
+	// Final declaration of jobs for the keys
 	public boolean keyUp(Event e, int key) {
 		if(key == 1006)
 			leftArrow = false;
@@ -181,7 +170,7 @@ public class Breakout extends Applet implements Runnable {
 		return true;
 	}
 
-
+	// Displays all sounds and tips that go along with winning
 	public void win() {
 		showStatus("Congradulations From Group 3! Your score was: " + score);
 		gContext.setColor(Breakout.BACKGROUND);
@@ -193,8 +182,8 @@ public class Breakout extends Applet implements Runnable {
 		tipWindow(tips());
 		score = 0;
 	}
-
-
+	
+	// Displays all sounds and tips that go along with losing
 	public void lose() {
 		if(numberlost < LIVES) {
 			Sound.miss.play();
@@ -229,19 +218,19 @@ public class Breakout extends Applet implements Runnable {
 	public void tipWindow(String[] s){
 		Sound.listen.play();
 		JTextArea text = new JTextArea();
-		if(score >= 0 && score < 10){
+		if(score >= 0 && score < 90){
 			text = new JTextArea(s[0] + "\n" + s[1] +  "\n" + s[2] + "\n" + s[3]);
 			scrollPane(text);
 		}
-		else if (score > 9 && score < 20){
+		else if (score > 91 && score < 180){
 			text = new JTextArea( s[0] + "\n" + s[1] + "\n" + s[2] + "\n" + s[3] + "\n" + s[4]);
 			scrollPane(text);
 		}
-		else if (score > 19 && score < 30){
+		else if (score > 181 && score < 270){
 			text = new JTextArea(s[0] + "\n" + s[1] + "\n" +  s[2] + "\n" + s[3] + "\n" + s[4] + "\n" + s[5]);
 			scrollPane(text);
 		}
-		else if (score > 29 && score < 40){
+		else if (score > 271 && score < 360){
 			text = new JTextArea( s[0] + "\n" + s[1] + "\n" + s[2] + "\n" + s[3] + "\n" + s[4] + "\n" + s[5]+ "\n" + s[6]);
 			scrollPane(text);
 		}
@@ -249,10 +238,12 @@ public class Breakout extends Applet implements Runnable {
 			text = new JTextArea(s[0] + "\n" + s[1] + "\n" + s[2] + "\n" + s[3] + "\n" + s[4] + "\n" + s[5]+ "\n" + s[6] + "\n" + s[7]);
 			scrollPane(text);
 		}
+		
+		Sound.backGround.loop();
 	}
 	
 	
-	
+	// MAkes our text area wrappable and scrollable
 	public void scrollPane(JTextArea textArea){
 		JScrollPane scrollPane = new JScrollPane(textArea);  
 		textArea.setLineWrap(true);  
@@ -266,9 +257,9 @@ public class Breakout extends Applet implements Runnable {
 	public String[] tips(){
 		String[] s = new String[8];
 
-s[0] = ("BSCIS Admission Requirements\n" +
-		        
-	">Firstly congratulations from group 3 on getting the a score of " + score + "\n" +
+s[0] = (">Firstly congratulations from group 3 on getting the a score of " + score + "\n\n\n" +
+		
+	"BSCIS Admission Requirements\n\n" +
 	">Students may declare CIS as their major as soon as they have met the program’s admission requirements:\n\t"+
 
 	">A 2.0 grade point average overall.\n\t "+
@@ -325,15 +316,17 @@ s[0] = ("BSCIS Admission Requirements\n" +
 		return s;
 	}
 
+	// Gets left arrow instructions
 	public boolean getLeftArrow() {
 		return leftArrow;
 	}
 
+	//gets right arrow instructions
 	public boolean getRightArrow() {
 		return rightArrow;
 	}
 	
-
+	// Randomly generates a color 
 	public static Color randomColorGen() {
 		Random rand = new Random();
 		int red = rand.nextInt(255);
@@ -342,7 +335,7 @@ s[0] = ("BSCIS Admission Requirements\n" +
 		return new Color(red, green, blue);
 	}
 	
-	
+	// Checks to make sure that all the items that require color have not generated the exact same color
 	public void colorCheck(){
 		Color[] colorA = new Color[9];
 		colorA[0] = BACKGROUND;
@@ -375,338 +368,4 @@ s[0] = ("BSCIS Admission Requirements\n" +
 	}
 }
 
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
 
-class BlockHolder {
-	private int blockCount;
-	protected static Line[] lines;
-	private int TARGETHEIGHT;
-
-	public BlockHolder() {
-		TARGETHEIGHT = Breakout.TARGETHEIGHT;
-		lines = new Line[6];
-		prepareBlocks();
-	}
-
-	
-	public void restart() {
-		blockCount = 0;
-		for(int i = 0; i < lines.length; i++) {
-			lines[i].restart();
-			blockCount += lines[i].numberblocks;
-		}
-	}
-
-
-	public void prepareBlocks() {
-		int spacing = Breakout.BLOCKSPACINGY;
-		lines[0] = new Line(0, Breakout.randomColorGen());
-		lines[1] = new Line(TARGETHEIGHT+spacing, Breakout.randomColorGen());
-		lines[2] = new Line(TARGETHEIGHT*2+2*spacing, Breakout.randomColorGen());
-		lines[3] = new Line(TARGETHEIGHT*3+3*spacing, Breakout.randomColorGen());
-		lines[4] = new Line(TARGETHEIGHT*4+4*spacing, Breakout.randomColorGen());
-		lines[5] = new Line(TARGETHEIGHT*5+5*spacing, Breakout.randomColorGen());
-		
-		for(int i = 0; i < lines.length; i++) {
-			blockCount += lines[i].numberblocks;
-			lines[i].fill();
-		}
-	}
-
-
-
-	public Block whohit(int x, int y) {
-		for(int i = 0; i < lines.length; i++) {
-			if(lines[i].bottom >= y && lines[i].top <= y) {
-				Block hit = lines[i].whohit(x);
-				if(hit != null)
-					blockCount -= 1;
-				return hit;
-			}
-		}
-		return null;
-	}
-
-	public void draw(Graphics g) {
-		for(int i = 0; i < lines.length; i++)
-			lines[i].draw(g);
-	}
-
-
-	public int blocksLeft() {
-		return blockCount;
-	}
-	
-}
-
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-
-class Line {
-	public int bottom, top, numberblocks, initial;
-	public Block[] blocks;
-	private boolean[] exists;
-	private int TARGETLENGTH;
-	Color color = Breakout.randomColorGen();
-
-
-	public Line(int top0, Color color0) {
-		top = top0;
-		color = color0;
-		TARGETLENGTH = Breakout.TARGETLENGTH;
-		bottom = top + Breakout.TARGETHEIGHT;
-		numberblocks = (int) Breakout.MAXX / TARGETLENGTH;
-		initial = (int) (Breakout.MAXX - TARGETLENGTH * numberblocks) / 2;
-		blocks = new Block[numberblocks];
-		exists = new boolean[numberblocks];
-	}
-	
-
-	public void fill() {
-		for(int i = 0; i < numberblocks; i++) {
-			blocks[i] = new Block(TARGETLENGTH * i + initial, top + 1, color);
-			exists[i] = true;
-		}
-	}
-
-	public void restart() {
-		for(int i = 0; i < numberblocks; i++) {
-			exists[i] = true;
-		}
-	}
-
-
-	public void draw(Graphics g) {
-		for(int i = 0; i < numberblocks; i++) {
-			if(exists[i])
-				blocks[i].draw(g);
-		}
-	}
-	
-
-	public Block whohit(int x) {
-		int numhit = (int) (x - initial) / TARGETLENGTH;
-		
-		if(numhit > numberblocks) 
-			numhit = numberblocks;
-
-		if(exists[numhit]) {
-			exists[numhit] = false;
-			return blocks[numhit];
-		}
-		return null;
-	}
-	
-}
-
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-
-class Block {
-	public int x, y;
-	protected Color color = Breakout.randomColorGen();
-	protected int MAXX, MAXY;
-
-
-	public Block(int x0, int y0, Color color0) {
-		x = x0;
-		y = y0;
-		color = color0;
-		MAXX = Breakout.TARGETLENGTH - Breakout.BLOCKSPACINGX / 2;
-		MAXY = Breakout.TARGETHEIGHT - Breakout.BLOCKSPACINGY / 2;
-	}
-
-
-	public void clearBlock(Graphics g) {
-		g.setColor(Breakout.BACKGROUND);
-		Breakout.score++;
-		g.fillRect(x, y, MAXX, MAXY);
-	}
-	
-
-	public void clearPaddle(Graphics g) {
-		g.setColor(Breakout.BACKGROUND);
-		g.fillRect(x, y, MAXX, MAXY);
-	}
-
-
-	public void draw(Graphics g) {
-		g.setColor(color);
-		g.fillRect(x, y, MAXX, MAXY);
-	}
-
-	public int getTop() {
-		return y;
-	}
-
-	public int getBottom() {
-		return y + MAXY;
-	}
-
-	public int getleftx() {
-		return x;
-	}
-
-	public int getrightx() {
-		return x + MAXX;
-	}
-
-}
-	
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
- 
-class Paddle extends Block {
-	public boolean moved = false;
-
-	public Paddle() {
-		super(Breakout.MAXX / 2 - Breakout.PADDLEWIDTH / 2, 
-			Breakout.PADDLEALTITUDE, 
-			Breakout.PADDLECOLOR);
-		MAXX = Breakout.PADDLEWIDTH;
-		MAXY = Breakout.PADDLEHEIGHT;
-
-	}
-
-
-	public void go(int x0, int y0) {
-		x=x0;
-		y=y0;
-	}
-	
-
-	public void move(int change) {
-		if((x+MAXX < Breakout.MAXX || change < 0) && (x > 0 || change > 0)) { 
-			x = x + change;
-			moved = true;
-		}
-		else
-			moved = false;
-	}
-
-}
-
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-
-class Ball {
-	public double xchange, ychange;
-	private double x, y;
-	private int radius;
-	private Color color = Breakout.BALLCOLOR;
-	private BlockHolder blocks;
-
-
-
-	public Ball(BlockHolder blocks0) {
-		xchange = 0;
-		ychange = 0;
-		radius = Breakout.BALLSIZE;
-		x = Breakout.MAXX / 2 - radius;
-		y = Breakout.PADDLEALTITUDE - 5;
-		color = Breakout.BALLCOLOR;
-		blocks = blocks0;
-	}
-
-	public void go(int x0, int y0) {
-		x = (double) x0;
-		y = (double) y0;
-		xchange = 0.0;
-		ychange = 0.0;
-	}
-
-	public void selfMove(Graphics g, Block paddle, Breakout ap) {
-		if(x + xchange >= Breakout.MAXX)
-			xchange =- Math.abs(xchange);
-		if(x + xchange <= 0)
-			xchange = Math.abs(xchange);
-		if(y + ychange <= 0)
-			ychange =- ychange;
-		if(paddle.getTop() <= y + ychange && paddle.getTop() >= y - ychange &&
-			x + xchange >= paddle.getleftx() && x + xchange <= paddle.getrightx()) {
-			ychange =- Math.abs(ychange);
-			if(ap.getLeftArrow()) {
-				xchange -= Breakout.XHIT_CHANGE;
-				ychange -= Breakout.YHIT_CHANGE;
-			}
-			if(ap.getRightArrow()) {
-				xchange += Breakout.XHIT_CHANGE;
-				ychange -= Breakout.YHIT_CHANGE;
-			}
-		}
-		if(y + ychange >= Breakout.MAXX)	{
-			ap.lose();
-		}
-		checkBlocks(g);
-
-		move(xchange, ychange, g);
-	}
-
-	public void move(double xchange, double ychange, Graphics g) {
-		x = x + xchange;
-		y = y + ychange;
-	}
-
-
-	public void checkBlocks(Graphics g) {
-		Block hit = blocks.whohit((int) (x + xchange), (int) (y + ychange));
-	
-		if(hit != null) {
-			hit.clearBlock(g);
-			Sound.hit.play();
-			ychange = -ychange;
-		}
-	}
-
-
-	public void clear(Graphics g) {
-		g.setColor(Breakout.BACKGROUND);
-		g.fillOval((int) x, (int) y, radius * 2,radius * 2);
-	}
-
-
-	public void draw(Graphics g) {
-		g.setColor(color);
-		g.fillOval((int) x,(int) y, radius * 2, radius * 2);
-	}
-}
-
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-//**********************************************************************************************************************
-
-class Sound extends Breakout{
-	
-	static AudioClip backGround, hit, miss, win, endGame, listen; // Sound player
-	URL url;
-	static Breakout bo;
-
-	// In order to get music to work use new Sound(this); and on next line use Sound.(sound wanting to play).play() or Loop()
-	public Sound(Breakout bo){
-		try {
-			url = bo.getDocumentBase();
-		}
-		catch(Exception e){
-		}
-		backGround = bo.getAudioClip(url, "Music/G3BackgroundMusic.au");
-		win = bo.getAudioClip(url, "Music/G3WinMusic.au");
-		hit = bo.getAudioClip(url, "Music/G3Hit.au");
-		endGame = bo.getAudioClip(url, "Music/smb_gameover.au");
-		listen = bo.getAudioClip(url, "Music/G3Info.au");
-		miss = bo.getAudioClip(url, "Music/G3Miss.au");
-	}
-	
-
-}
